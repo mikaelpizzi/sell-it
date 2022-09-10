26d973dfb98d274f7cebb3788349130633f57a38
Mikael <mikaelangelopizzi@gmail.com>
2022-09-09 20:37:28 -0400

    [IMP] Add tests for product controller

---
+++ test/controllers/products_controller_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+class ProductsControllerTest < ActionDispatch::IntegrationTest
+    test "Render a list of products" do
+        get products_path
+
+        assert_response :success
+        assert_select ".product", 2
+    end
+
+    test "Render a detail product page" do
+        get product_path(products(:redmi))
+
+        assert_response :success
+        assert_select ".title", "Redmi K40"
+        assert_select ".description", "Cool phone 12gb ram 256gb rom, like new"
+        assert_select ".price", "380$"
+    end
+end
