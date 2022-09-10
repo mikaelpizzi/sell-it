c2fb7b0b2377ebf758066d54455a65316d13603a
Mikael <mikaelangelopizzi@gmail.com>
2022-09-10 00:36:40 -0400

    [IMP] Add test to create new product form

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -23,4 +23,16 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
         assert_response :success
         assert_select "form"
     end
+
+    test "Allow to create a new product" do
+        post products_path, params: {
+            product: {
+                title: "Nintendo 64",
+                description: "Used, but still playable",
+                price: 45
+            }
+        }
+        
+        assert_redirected_to products_path
+    end
 end
