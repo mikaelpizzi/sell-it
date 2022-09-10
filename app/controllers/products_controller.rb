2ada7195061708dcfccd7eb0d71976027d8ad38c
Mikael <mikaelangelopizzi@gmail.com>
2022-09-10 00:29:16 -0400

    [ADD] Add method to save new product via POST

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,6 +10,20 @@ class ProductsController < ApplicationController
     def new
         @product = Product.new
     end
+
+    def create
+        @product = Product.new(product_params)
+
+        if @product.save
+            redirect_to products_path
+        else
+            render :new
+        end
+    end
+
+    def product_params
+        params.require(:product).permit(:title, :description, :price)
+    end
 end
 
 # Modelo: Todo con respecto a la base de datos, consultas, validaciones, relaciones
