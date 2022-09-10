ffef4145f7c27b92e0273983b8c47fad1a2d03d2
Mikael <mikaelangelopizzi@gmail.com>
2022-09-10 18:18:08 -0400

    [ADD] Add photo field on create product, display photos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -46,7 +46,7 @@ class ProductsController < ApplicationController
     private
 
     def product_params
-        params.require(:product).permit(:title, :description, :price)
+        params.require(:product).permit(:title, :description, :price, :photo)
     end
 end
 
